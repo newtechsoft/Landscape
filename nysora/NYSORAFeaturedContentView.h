9ee3d3fd7d6418411b670f333bec56dbaf7dca8b
Johan Hörnell <johan.hornell@hypo.se>
2014-09-02 07:35:19 +0200

    constrained featured content text to lower third

--- nysora/NYSORAFeaturedContentView.h
+++ nysora/NYSORAFeaturedContentView.h
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) UIPageControl *pageControl;
 @property (nonatomic, strong) UIImageView *contentImage;
 @property (nonatomic, strong) UITextView *contentText;
+@property (nonatomic, strong) UIView *contentTextBackground;
 @property (nonatomic, strong) NSMutableArray *featuredContent;
 
 - (id)initWithFeaturedContent:(NSMutableArray *)content frame:(CGRect)frame;
